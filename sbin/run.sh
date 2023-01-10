7e3e2261fdb7178f8cdf26bd8c5b602be5f3ad6b
LeeTaehee <prussian1933@naver.com>
2023-01-10 14:36:13 +0900

    Hotfix +updated: run script path

--- sbin/run.sh
+++ sbin/run.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-sudo docker build -t node:custom /home/ec2-user/server-test/app
+sudo docker build -t node:custom ~/server-test/app
 sudo docker run -d -it -u root -p 8080:8080 --name app_container node:custom /bin/bash
